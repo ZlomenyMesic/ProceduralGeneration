862affba012d2ef1f99d29535ffb680d4583dda3
ZlomenyMesic <michna.danecek@gmail.com>
2023-11-14 16:55:01 +0100

    added Colors.cs

---
+++ minecraft_kurwa/Colors.cs
@@ -0,0 +1,19 @@
+﻿//
+// minecraft_kurwa
+// ZlomenyMesic, KryKom
+//
+
+using Microsoft.Xna.Framework;
+
+namespace minecraft_kurwa {
+    internal static class Colors {
+        internal static readonly Color GRASS_COLD = new(19, 109, 21);
+        internal static readonly Color GRASS_NORMAL = new(19, 133, 16);
+        internal static readonly Color GRASS_WARM = new(65, 152, 10);
+        internal static readonly Color GRASS_DRY = new(120, 144, 48);
+
+        internal static readonly Color DIRT_DRY = new(144, 120, 48);
+
+        internal static readonly Color ROCK = new(103, 146, 125);
+    }
+}
