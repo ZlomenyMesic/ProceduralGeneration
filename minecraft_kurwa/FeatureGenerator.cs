00a2f4524ac198683256deaeb8f310db623422fc
ZlomenyMesic <michna.danecek@gmail.com>
2023-11-17 16:28:20 +0100

    added FeatureGenerator.cs

---
+++ minecraft_kurwa/FeatureGenerator.cs
@@ -0,0 +1,28 @@
+﻿//
+// minecraft_kurwa
+// ZlomenyMesic, KryKom
+//
+
+using System;
+
+namespace minecraft_kurwa {
+    internal class FeatureGenerator {
+        public static class Rock {
+            public static void GenerateRock(int size, int posX, int posY) {
+                for (int x = posX - size / 2; x < posX + size / 2; x++) {
+                    for (int y = posY - size / 2; y < posY + size / 2; y++) {
+                        for (int z = Global.HEIGHT_MAP[x, y] - 2 - size / 2; z < Global.HEIGHT_MAP[x, y] - 2 + size / 2; z++) {
+                            if (PseudoCircle(x, y, z)) {
+                                Global.VOXEL_MAP[x, y, z] = VoxelType.STONE;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool PseudoCircle(int x, int y, int z) {
+            return Math.Round(Math.Pow(x, 2) / Math.Pow(z, 2) - Math.Pow(z, 2)) < y && y < Math.Round(Math.Pow(x, 2) / (-Math.Pow(z, 2)) + Math.Pow(z, 2));
+        }
+    }
+}
