a8be6e44876fcf5ae4e364b5582b56f36386c4e8
krystof <brv@brv.brv>
2023-11-25 07:26:27 +0100

    Renamed VoxelStructure.cs to Voxel.cs and moved enum VoxelType into it

--- minecraft_kurwa/Global.cs
+++ minecraft_kurwa/Global.cs
@@ -47,40 +47,4 @@ namespace minecraft_kurwa {
         internal const int TREE_DENSITY = 130;
         internal const int TREE_EDGE_OFFSET = 0; // trees on the edge will be cut
     }
-
-    internal enum VoxelType {
-        UNKNOWN = 0,
-        GRASS = 1,
-        STONE = 2,
-        SAND = 3,
-        ICE = 4,
-        TERRACOTTA = 5,
-        GRAVEL = 6,
-        SNOW = 7,
-
-        OAK_LEAVES = 8,
-        OAK_WOOD = 9,
-
-        KAPOK_LEAVES = 10,
-        KAPOK_WOOD = 11,
-
-        SPRUCE_LEAVES = 12,
-        SPRUCE_WOOD = 13,
-
-        BEECH_LEAVES = 14,
-        BEECH_WOOD = 15,
-
-        MAPLE_LEAVES = 16,
-        MAPLE_WOOD = 17,
-
-        WATER = 18,
-
-        POPLAR_LEAVES = 19,
-        POPLAR_WOOD = 20,
-
-        CHERRY_LEAVES = 21,
-        CHERRY_WOOD = 22,
-        
-        SANDSTONE = 23,
-    }
 }
