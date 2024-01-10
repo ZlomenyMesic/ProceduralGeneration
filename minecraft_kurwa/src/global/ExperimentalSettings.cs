0cc073ec7fd43e6c44ced37a360071e8e3bb001c
Daniel Michna <michna.19f@student.gymlit.cz>
2024-01-10 18:54:15 +0100

    cock

--- minecraft_kurwa/src/global/ExperimentalSettings.cs
+++ minecraft_kurwa/src/global/ExperimentalSettings.cs
@@ -22,7 +22,7 @@ internal static class ExperimentalSettings {
     internal static bool INVERT_COLORS = false; // nightmare fuel
 
     // if true, Settings.SEED is selected randomly
-    internal static bool RANDOM_SEED = false;
+    internal static bool RANDOM_SEED = true;
 
     // perlin noise output near [0, 0] is always the same
     // must be AT LEAST 30 to not generate any gravel around [0, 0]
